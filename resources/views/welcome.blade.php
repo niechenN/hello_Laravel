fc175cfc6bd0310c5a15eb0a56182ba246ef5a32
2018 <niechen1510@gmail.com>
2018-09-06 08:47:49 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+            }
+
+            .container {
+                margin: 50px auto;
+                text-align: center;
+                vertical-align: middle;
+            } 
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                text-align:center;
+                font-size: 66px;
+            }
+            
+        </style>
+    </head>
+    <body>
+        <div class="container">
+           <div class="content">
+               <div class="title">Hello Laravel! - by Nech</div>
+           </div> 
+        </div>
+    </body>
+</html>
